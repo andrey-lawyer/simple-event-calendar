5d7c72e5d2fdeef9b58bdeb3e44ed122b56cd670
andrey-lawyer <andrey-1@ukr.net>
2024-01-20 13:13:28 +0200

    small changes

--- services/apiGetAllEvents.ts
+++ services/apiGetAllEvents.ts
@@ -1,35 +1,6 @@
-// import dbConnect from "@/lib/dbConnect";
-// import Event from "@/models/Event";
-// import { IEvents } from "@/types/eventForm";
-// import { getIronSession } from "iron-session";
-import { headers, cookies } from "next/headers";
-
-// async function getIronSessionCustom() {
-//   return await getIronSession(cookies(), {
-//     password: process.env.SESSION_SECRET,
-//     cookieName: "cookieName",
-//   });
-// }
+import { headers } from "next/headers";
 
 export const getAllEvents = async () => {
-  //
-  //   await dbConnect();
-  //   const session = (await getIronSessionCustom()) as any;
-  //   console.log(session);
-  //   if (!session?.user) {
-  //     console.log("User not authenticated");
-  //   }
-
-  //   const userIdGet = session.user.userId;
-
-  //   try {
-  //     const events = await Event.find({ user: userIdGet });
-  //     return { data: events };
-  //   } catch (error) {
-  //     console.log("Internal Server Error: Unable to fetch data");
-  //   }
-  // };
-
   try {
     // console.log(process.env.BACKEND_API);
     const response = await fetch(`${process.env.BACKEND_API}events`, {
