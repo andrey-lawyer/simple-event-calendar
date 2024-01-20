190df523374a40857ad7ab9ecc8f59dd81021d53
andrey-lawyer <andrey-1@ukr.net>
2024-01-20 13:22:46 +0200

    change function getEvents

--- services/apiGetAllEvents.ts
+++ services/apiGetAllEvents.ts
@@ -1,19 +1,47 @@
-import { headers } from "next/headers";
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+
+import dbConnect from "@/lib/dbConnect";
+import Event from "@/models/Event";
+
+async function getIronSessionCustom() {
+  return await getIronSession(cookies(), {
+    password: process.env.SESSION_SECRET,
+    cookieName: "cookieName",
+  });
+}
 
 export const getAllEvents = async () => {
+  await dbConnect();
+  const session = (await getIronSessionCustom()) as any;
+  console.log(session);
+  if (!session?.user) {
+    console.log("User not authenticated");
+  }
+
+  const userIdGet = session.user.userId;
+
   try {
-    // console.log(process.env.BACKEND_API);
-    const response = await fetch(`${process.env.BACKEND_API}events`, {
-      method: "GET",
-      headers: headers(),
-      cache: "no-store",
-    });
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
-    return response.json();
+    const events = await Event.find({ user: userIdGet });
+    return { data: events };
   } catch (error) {
-    console.error("Error fetching events:", error);
-    return null;
+    console.log("Internal Server Error: Unable to fetch data");
   }
 };
+
+//   // try {
+//   //   // console.log(process.env.BACKEND_API);
+//   //   const response = await fetch(`${process.env.BACKEND_API}events`, {
+//   //     method: "GET",
+//   //     headers: headers(),
+//   //     cache: "no-store",
+//   //   });
+//   //   if (!response.ok) {
+//   //     throw new Error("Failed to fetch data");
+//   //   }
+//   //   return response.json();
+//   // } catch (error) {
+//   //   console.error("Error fetching events:", error);
+//   //   return null;
+//   // }
+// };
