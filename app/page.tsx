500b5bffb30284f9b194374b4b4023376020fb67
andrey-lawyer <andrey-1@ukr.net>
2024-01-22 12:11:32 +0200

    small changes

--- app/page.tsx
+++ app/page.tsx
@@ -25,23 +25,25 @@ export default function Home() {
       <div className="relative">
         <div className="">
           {emailUser && (
-            <h1 className="font-inter text-center font-medium my-10  text-xl px-4 leading-9 ">
-              {emailUser}
-              <span className="block">
-                Welcome to the Event Calendar! This is the main page where you
-                can manage and view your events. Explore your schedule, add or
-                remove events, and stay organized. We hope you have a great
-                experience!
-              </span>
-            </h1>
+            <>
+              <h1 className="font-inter text-center font-medium my-10  text-xl px-4 leading-9 ">
+                {emailUser}
+                <span className="block">
+                  Welcome to the Event Calendar! This is the main page where you
+                  can manage and view your events. Explore your schedule, add or
+                  remove events, and stay organized. We hope you have a great
+                  experience!
+                </span>
+              </h1>
+              <Link
+                className="flex items-center justify-center gap-2 font-inter hover:text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out"
+                href="/events"
+              >
+                <FaRegArrowAltCircleRight className="w-10 h-10" />
+                <span className="text-lg">Go to Calendar</span>
+              </Link>
+            </>
           )}
-          <Link
-            className="flex items-center justify-center gap-2 font-inter hover:text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300 ease-in-out"
-            href="/events"
-          >
-            <FaRegArrowAltCircleRight className="w-10 h-10" />
-            <span className="text-lg">Go to Calendar</span>
-          </Link>
         </div>
       </div>
     </main>
